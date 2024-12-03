ec4895ba9e8ea569b9a734af67ea278c24e09c53
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-12-03 04:08:02 -0800

    Hamburgur menu

--- src/components/hero-section/Hero.tsx
+++ src/components/hero-section/Hero.tsx
@@ -3,7 +3,7 @@
 export default function Hero() {
   // const navigate = useNavigate();
   return (
-    <section className="~pt-20/4 border-2 border-black">
+    <section className="~pt-20/4">
       <div className="flex flex-col-reverse mx-auto w-full ~gap-x-2/10 items-center ~px-1/6 max-w-7xl md:flex-row">
         <div className="sm:w-1/2">
           <img
@@ -11,12 +11,6 @@ export default function Hero() {
             alt="Music Listening mobile"
             className="w-full mx-auto"
           />
-
-          {/* <img
-            src="../../../public/images/hero-012.png"
-            alt="Music Listening desktop"
-            className="hidden sm:block"
-          /> */}
         </div>
 
         <div className="p-4 text-center sm:w-1/2 md:text-left">
@@ -36,34 +30,3 @@ export default function Hero() {
     </section>
   );
 }
-
-//// <section className="flex flex-col ~gap-14/16 pt-12 border md:items-center md:flex-row">
-//   <div className="w-full">
-//     {/* <img
-//       loading="lazy"
-//       src="./public/images/hero-01.png"
-//       alt="Hero section picture"
-//       className="w-full h-[400px] sm:h-4/5"
-//     /> */}
-//     <HeroSvg />
-//   </div>
-//   <div className="px-4 pb-8 text-[#121212]">
-//     <h1 className="mb-6 ~text-4xl/7xl capitalize w-2/3">
-//       Listen to the <span className="text-[#377DFF]">amazing</span>music
-//       sound.
-//     </h1>
-//     <p className="~text-sm/xl text-neutral-03 leading-8 md:w-96 mb-4">
-//       Everyone needs a good winter jacket. Find yours with our collection
-//       and more.
-//     </p>
-
-//     <button
-//       onClick={() => {
-//         navigate("/shop");
-//       }}
-//       className="py-3 text-white transition-all duration-200 rounded-lg px-14 bg-secondary-blue w-fit h-fit hover:bg-blue-800"
-//     >
-//       shopping now
-//     </button>
-//   </div>
-// </section>
--- src/ui/header/Tools.tsx
+++ src/ui/header/Tools.tsx
@@ -1,5 +1,7 @@
 import { CartIcon, ProfileIcon, SearchIcon } from "../../assets/icons";
 
+import { HamburgerMenu } from "../../ui/HamburgerMenu";
+
 export default function Tools() {
   return (
     <ul className="flex items-center gap-4">
@@ -10,6 +12,8 @@ export default function Tools() {
         <span className="flex items-center justify-center w-5 h-5 p-2 text-sm text-white bg-black rounded-full">
           2
         </span>
+
+        <HamburgerMenu />
       </div>
     </ul>
   );
