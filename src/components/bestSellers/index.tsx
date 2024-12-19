12fb06bb502f47deca86df081c3312c6cae312aa
RamtinRmzani <Ramtinramzani76@gmail.com>
2024-12-19 04:10:45 -0800

    add show more

--- src/components/bestSellers/index.tsx
+++ src/components/bestSellers/index.tsx
@@ -3,45 +3,12 @@ import { CartHover } from "../../ui/cart/Cart";
 import CartHeader from "../../ui/cart/CartHeader";
 import MainContainer from "../../ui/MainContainer";
 
+import data from "../../../data/cart.json";
+import { useState } from "react";
+
 export default function BestSeller() {
-  const cart = [
-    {
-      id: 1,
-      src: "/images/headphone-01.png",
-      title: "airpod",
-      detail: "new",
-      description: "JBL Reflect Flow Pro+ Bluetooth Truly Wireless Sports",
-      price: 20000,
-      rating: 4,
-    },
-    {
-      id: 2,
-      src: "/images/airpod-01.png",
-      title: "airpod",
-      detail: "new",
-      description: "JBL Reflect Flow Pro+ Bluetooth Truly Wireless Sports",
-      price: 300,
-      rating: 4,
-    },
-    {
-      id: 3,
-      src: "/images/airpod-02.png",
-      title: "airpod",
-      detail: "hot",
-      description: "JBL Reflect Flow Pro+ Bluetooth Truly Wireless Sports",
-      price: 300,
-      rating: 1,
-    },
-    {
-      id: 4,
-      src: "/images/headphone-03.png",
-      title: "airpod",
-      detail: "new",
-      description: "Test",
-      price: 600,
-      rating: 3,
-    },
-  ];
+  const cart = data.carts;
+  const [cartView] = useState(4);
 
   return (
     <section>
@@ -49,7 +16,7 @@ export default function BestSeller() {
         <CartHeader title="best seller" />
 
         <div className="grid sm:grid-cols-2 lg:grid-cols-4 gap-x-8 gap-y-12">
-          {cart.map((item) => (
+          {cart.slice(0, cartView).map((item) => (
             <CartHover
               src={item.src}
               title={item.title}
--- src/components/shop/MainShop.tsx
+++ src/components/shop/MainShop.tsx
@@ -4,16 +4,18 @@ import Filter from "./Filter";
 import CategoriesFilter from "./CategoriesFilter";
 import PriceFIlter from "./PriceFilter";
 import { CartHover } from "../../ui/cart/Cart";
-import Button from "../../ui/Button";
+
+// import Test from "./Test";
+import data from "../../../data/cart.json";
 
 export default function MainShop() {
-  const [catFilter, setCatFilter] = useState([
-    { id: 1, text: "living room" },
-    { id: 2, text: "bedroom" },
-    { id: 3, text: "kitchen" },
-    { id: 4, text: "bathroom" },
-    { id: 5, text: "dinning" },
-    { id: 6, text: "outdoor" },
+  const [catFilter] = useState([
+    { id: 1, text: "headphone" },
+    { id: 2, text: "headset" },
+    { id: 3, text: "airpod" },
+    { id: 4, text: "mobile" },
+    { id: 5, text: "test" },
+    { id: 6, text: "test" },
     { id: 7, text: "test" },
     { id: 8, text: "test" },
     { id: 9, text: "test" },
@@ -24,101 +26,14 @@ export default function MainShop() {
     { id: 3, text: "$200.00 - $299.99" },
     { id: 4, text: "$400.00+" },
   ]);
-
-  const cart = [
-    {
-      id: 1,
-      src: "/images/headphone-01.png",
-      title: "airpod",
-      detail: "new",
-      description: "JBL Reflect Flow Pro+ Bluetooth Truly Wireless Sports",
-      price: 20000,
-      discount: 20,
-      rating: 4,
-    },
-    {
-      id: 2,
-      src: "/images/airpod-01.png",
-      title: "airpod",
-      detail: "new",
-      description: "JBL Reflect Flow Pro+ Bluetooth Truly Wireless Sports",
-      price: 300,
-      discount: 50,
-      rating: 4,
-    },
-    {
-      id: 3,
-      src: "/images/airpod-02.png",
-      title: "airpod",
-      detail: "hot",
-      description: "JBL Reflect Flow Pro+ Bluetooth Truly Wireless Sports",
-      price: 300,
-      discount: 10,
-      rating: 1,
-    },
-    {
-      id: 4,
-      src: "/images/headphone-03.png",
-      title: "airpod",
-      detail: "new",
-      description: "Test",
-      discount: 70,
-      price: 600,
-      rating: 3,
-    },
-    {
-      id: 5,
-      src: "/images/headphone-04.png",
-      title: "airpod",
-      detail: "new",
-      description: "Test",
-      discount: 70,
-      price: 600,
-      rating: 3,
-    },
-    {
-      id: 6,
-      src: "/images/headphone-05.png",
-      title: "airpod",
-      detail: "new",
-      description: "Test",
-      discount: 70,
-      price: 600,
-      rating: 3,
-    },
-    {
-      id: 7,
-      src: "/images/headphone-01.png",
-      title: "airpod",
-      detail: "new",
-      description: "Test",
-      discount: 70,
-      price: 600,
-      rating: 3,
-    },
-    {
-      id: 8,
-      src: "/images/headphone-04.png",
-      title: "airpod",
-      detail: "new",
-      description: "Test",
-      discount: 70,
-      price: 600,
-      rating: 3,
-    },
-    {
-      id: 9,
-      src: "/images/headphone-03.png",
-      title: "airpod",
-      detail: "new",
-      description: "Test",
-      discount: 70,
-      price: 600,
-      rating: 3,
-    },
-  ];
-
   const [selectCat, setSelectCat] = useState<string[]>([catFilter[0].text]);
+
+  // Carts
+  const cart = data.carts;
+  const [visibleCart, setVisibleCart] = useState(9);
+  function showMoreProduct() {
+    setVisibleCart((prev) => prev + 6);
+  }
 
   return (
     <div className="flex mt-16 gap-14">
@@ -146,11 +61,11 @@ export default function MainShop() {
       </div>
       <div className="flex flex-col w-3/4">
         <div className="flex justify-between">
-          <h2 className="text-lg font-semibold">{selectCat}</h2>
+          <h2 className="text-lg font-semibold uppercase">{selectCat}</h2>
           <div className="w-1/3 h-10"></div>
         </div>
         <div className="grid grid-cols-3 grid-rows-3 mt-6 gap-x-6 gap-y-8">
-          {cart.map((item) => (
+          {cart.slice(0, visibleCart).map((item) => (
             <CartHover
               src={item.src}
               title={item.title}
@@ -164,9 +79,14 @@ export default function MainShop() {
           ))}
         </div>
 
-        <button className="self-center py-1 mt-10 border-2 border-gray-500 text-neutral-06 hover:font-semibold hover:border-gray-700 w-36 rounded-xl">
-          Show more
-        </button>
+        {visibleCart < cart.length && (
+          <button
+            className="self-center py-1 mt-10 border-2 border-gray-500 text-neutral-06 hover:font-semibold hover:border-gray-700 w-36 rounded-xl"
+            onClick={showMoreProduct}
+          >
+            Show more
+          </button>
+        )}
       </div>
     </div>
   );
