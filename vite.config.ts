0c179bd89522b921685e1cd5e1f750d7c6731234
Mahdi-Moosavi <mahdi.moosavi@hitaxi.cab>
2025-01-03 22:26:23 +0330

    PWA added

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "RM-Ecommerce",
+        short_name: "RM-Shop",
+        description: "A progressive web app built with Vite",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#FFC93C",
+        theme_color: "#FFC93C",
+        icons: [
+          {
+            src: "/logo.png",
+            sizes: "260x260",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
